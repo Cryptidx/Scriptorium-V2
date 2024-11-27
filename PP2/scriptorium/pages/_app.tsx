bd506a0974b50f399c3e41de513a90dfb6611f9a
hannahomeiza <omeiza81@gmail.com>
2024-11-27 04:36:00 -0500

    conflict with app

--- PP2/scriptorium/pages/_app.tsx
+++ PP2/scriptorium/pages/_app.tsx
@@ -2,13 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@/context/userContext";
 import { ThemeProvider } from "@/context/modeContext"; // Adjust the path as necessary
+import { DropdownProvider} from "@/components/drop-downs/dropdownContext";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <DropdownProvider>
+      <ThemeProvider>
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
     </ThemeProvider>
+    </DropdownProvider>
+    
   );
 }
