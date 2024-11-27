fc2aff0516d4ff97321aa951d6a17b84a3a595cf
hannahomeiza <omeiza81@gmail.com>
2024-11-27 16:44:55 -0500

    putting the components into tsx file

--- PP2/scriptorium/components/header.tsx
+++ PP2/scriptorium/components/header.tsx
@@ -79,58 +79,22 @@ import SearchBar from "./searchBar";
 import PageDropDown from "@/components/drop-downs/pages-dropdown";
 import Dropdown from "@/components/drop-downs/dropDown";
 import { DropdownProvider } from "./drop-downs/dropdownContext";
+import { MoonIcon, SunIcon } from "@/common/icons";
+import { DropdownItem } from '../types/dropdown.d';
 
-// Declare the type inline for the icons
-type IconProps = React.FC<React.SVGProps<SVGSVGElement>>;
-
-const SunIcon: IconProps = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-    />
-  </svg>
-);
-
-const MoonIcon: IconProps = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-    />
-  </svg>
-);
-
-interface DropdownItem {
-  label: string;
-  link: string;
-}
-
-// const userItems: DropdownItem[] = [
-//   { label: "Profile", link: "/home" },
-//   { label: "Settings", link: "/settings" },
-//   { label: "Logout", link: "/" },
-// ];
 
 const userItems: DropdownItem[] = [
   { label: "Profile", link: "/home" },
   { label: "Logout", link: "/" },
 ];
 
-const visitorItems: DropdownItem[] = [
-  { label: "Settings", link: "/settings" },
-];
+
 
 // const [activeDropdown, setActiveDropdown] = useState<string | null>(null); // Tracks which dropdown is open
 
 // const handleDropdownToggle = (dropdownName: string) => {
 //   setActiveDropdown((prev) => (prev === dropdownName ? null : dropdownName));
 // };
-
-
-
 
 
 const Header: React.FC<{ showSearchBar?: boolean }> = ({ showSearchBar = false }) => {
@@ -173,22 +137,7 @@ const Header: React.FC<{ showSearchBar?: boolean }> = ({ showSearchBar = false }
 
             {/* Navigation */}
             <nav className="flex items-center space-x-5">
-                {/* <Link href="/home">Home</Link>
-                <Link href="/codeEditor">Code</Link> */}
-                {/* <PageDropDown 
-                src="/icons/menu.png"
-                
-                items={[
-        { label: "Home", link: "/home" },
-        { label: "Code", link: "/codeEditor" },
-        { label: "Templates", link: "/templates" }
-        ]} 
-        isProfile="0"/> */}
-
-        {/* <DropdownProvider>
-        
-        </DropdownProvider>
-         */}
+       
         <PageDropDown
         id="burgerDropdown"
           trigger={<img src="/icons/menu.png" className="object-scale-down h-10 w-10" />}
@@ -203,9 +152,6 @@ const Header: React.FC<{ showSearchBar?: boolean }> = ({ showSearchBar = false }
           trigger={<img src="/avatar_images/pfp1.png" className="object-scale-down h-10 w-10 rounded-full" />}
           items= {login ? userItems : null }
         />
-
-        
-                
 
                 {/* Dynamic Button */}
                 {login ? (
