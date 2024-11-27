2c04f8e34b9c67b4b3224c49fe9883f051724987
karkabbage <limkaryna@gmail.com>
2024-11-27 03:26:49 -0500

    app update

--- PP2/scriptorium/pages/_app.tsx
+++ PP2/scriptorium/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@/context/userContext";
 import { ThemeProvider } from "@/context/modeContext"; // Adjust the path as necessary
+import { DropdownProvider } from "@/components/drop-downs/dropdownContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <DropdownProvider>
     <ThemeProvider>
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
     </ThemeProvider>
+    </DropdownProvider>
   );
 }
