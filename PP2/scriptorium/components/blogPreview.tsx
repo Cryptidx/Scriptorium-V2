2ad3d0b57f36178c4f816bc4b1bfe39bed2ba759
Gregory <gregory.gismondi@mail.utoronto.ca>
2024-11-28 16:33:54 -0500

    Added conditional de-link delete button

--- PP2/scriptorium/components/blogPreview.tsx
+++ PP2/scriptorium/components/blogPreview.tsx
@@ -1,3 +1,5 @@
+import { apiCallText } from "@/utils/auth-api-w-refresh-text";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface BlogPreviewProps {
@@ -6,12 +8,32 @@ interface BlogPreviewProps {
   author: string;
   tags: string[];
   language?: string;
+  blogId?: string;
+  tempId?: string;
 }
 
-const BlogPreview: React.FC<BlogPreviewProps> = ({ title, description, author, tags,language }) => {
+const BlogPreview: React.FC<BlogPreviewProps> = ({ title, description, author, tags, language, blogId, tempId }) => {
+
+  const router = useRouter();
+
+  const handleDelete = () => {
+    apiCallText(`/api/blog/` + blogId + `/template/` + tempId, {
+      method: "DELETE"
+    });
+
+    router.reload();
+  };
+
   return (
     // comment 
     <button className="relative p-4 border border-gray-300 rounded-md shadow-sm hover:shadow-md transition-shadow bg-gray-50">
+      {tempId && <button
+        onClick={handleDelete}
+        className="absolute top-2 left-2 p-2 bg-red-500 text-white rounded-full hover:bg-red-600 focus:outline-none"
+      >
+        🗑️
+      </button>}
+
       {language && (
         <div className="flex flex-row justify-end ">
             <div className="w-[25%] bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-md shadow-sm">
